2fbf42b4e952f5f5857499a8db5b37d5414f211c
Oliver Schmitz <o.schmitz@uu.nl>
2021-11-15 15:30:06 +0100

    Updated feedstock to version 0.3.1

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,20 +7,45 @@ cd $SRC_DIR
 
 mkdir -p build && cd build
 
+# -D CMAKE_C_COMPILER=${CC} \
+# -D CMAKE_CXX_COMPILER=${CXX} \
 
 cmake $SRC_DIR -G"Ninja" \
 -D CMAKE_BUILD_TYPE=Release \
 -D CMAKE_PREFIX_PATH:PATH="${PREFIX}" \
 -D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+-D CMAKE_INSTALL_LIBDIR=lib \
+-D Python3_EXECUTABLE="${PYTHON}" \
+-D LUE_HAVE_BOOST:BOOL=TRUE \
+-D LUE_HAVE_FMT:BOOL=TRUE \
+-D LUE_HAVE_PYBIND11:BOOL=TRUE \
+-D LUE_HAVE_HDF5:BOOL=TRUE \
+-D LUE_HAVE_GDAL:BOOL=TRUE \
+-D LUE_HAVE_DOCOPT:BOOL=TRUE \
+-D LUE_HAVE_GLEW:BOOL=TRUE \
+-D LUE_HAVE_GLFW:BOOL=TRUE \
+-D LUE_HAVE_MS_GSL:BOOL=FALSE \
+-D LUE_HAVE_NLOHMANN_JSON:BOOL=TRUE \
 -D LUE_DATA_MODEL_WITH_PYTHON_API=ON \
 -D LUE_DATA_MODEL_WITH_UTILITIES=ON \
 -D LUE_BUILD_VIEW=ON \
--D Python3_FIND_STRATEGY="LOCATION" \
--D Python3_EXECUTABLE="${PYTHON}" \
--D PYTHON_EXECUTABLE="${PYTHON}" \
--D Python_ROOT_DIR="${PREFIX}/bin" \
--D Python3_ROOT_DIR="${PREFIX}/bin"
+-D LUE_BUILD_TEST=ON \
+-D LUE_BUILD_FRAMEWORK=ON \
+-D LUE_FRAMEWORK_WITH_PYTHON_API=ON \
+-D LUE_BUILD_HPX=ON \
+-D HPX_USE_CMAKE_CXX_STANDARD=ON \
+-D HPX_WITH_MALLOC="tcmalloc" \
+-D HPX_WITH_PKGCONFIG=OFF \
+-D HPX_WITH_EXAMPLES=OFF \
+-D HPX_WITH_TESTS=OFF
 
-cmake --build . --target all
+# Use parallel build but not for lue.framework
+cmake --build . --target lue_view lue_translate lue_validate core
 
-cmake --build . --target install
+cmake --build . --target all --parallel 1
+
+export LD_PRELOAD=${PREFIX}/lib/libtcmalloc_minimal.so.4
+
+ctest --extra-verbose --output-on-failure
+
+cmake --install .
