da1f0691c2dc41c1fafca871dc17dad5ae254b7d
Kor de Jong <k.dejong1@uu.nl>
2022-10-28 14:50:59 +0200

    Fix builds, take I

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,11 +9,16 @@ set -x
 mkdir $HOME/lue_build && cd $HOME/lue_build
 
 if [[ "$target_platform" == "osx-64" ]]; then
-    export CXXFLAGS="${CXXFLAGS} -DTARGET_OS_OSX -D_LIBCPP_DISABLE_AVAILABILITY"
+    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
+if [[ "$target_platform" == "osx-64" || "$target_platform" == "osx-arm64" ]]; then
+    export CXXFLAGS="${CXXFLAGS} -DTARGET_OS_OSX"
 fi
 
 cmake $SRC_DIR -G"Ninja" \
     ${CMAKE_ARGS} \
+    -D CMAKE_BUILD_TYPE="Release" \
     -D LUE_INSTALL_PYTHON_PACKAGE_DIR="${SP_DIR}/lue" \
     -D LUE_HAVE_BOOST:BOOL=TRUE \
     -D LUE_HAVE_FMT:BOOL=TRUE \
