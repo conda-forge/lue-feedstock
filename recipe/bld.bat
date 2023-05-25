5a7671d0c4027151fdb9ce8cec4883fbea64e8ae
Kor de Jong <k.dejong1@uu.nl>
2023-05-25 16:18:38 +0200

    Merged in updates from LUE repository

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,19 +1,16 @@
 mkdir build
 cd build
-if %errorlevel% neq 0 exit /b %errorlevel%
+if errorlevel 1 exit 1
 
-cmake %SRC_DIR% ^
-    -G "Ninja" ^
+cmake .. ^
+    -G "NMake Makefiles" ^
     -D CMAKE_BUILD_TYPE=Release ^
     -D CMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
     -D CMAKE_INSTALL_LIBDIR=lib ^
     -D LUE_INSTALL_PYTHON_PACKAGE_DIR="%SP_DIR%/lue" ^
-    -D LUE_BUILD_DATA_MODEL=TRUE ^
     -D LUE_DATA_MODEL_WITH_PYTHON_API=TRUE ^
     -D LUE_DATA_MODEL_WITH_UTILITIES=TRUE ^
-    -D LUE_BUILD_FRAMEWORK=TRUE ^
     -D LUE_FRAMEWORK_WITH_PYTHON_API=TRUE ^
-    -D LUE_BUILD_VIEW=FALSE ^
     -D LUE_BUILD_QA=TRUE ^
     -D LUE_QA_WITH_PYTHON_API=TRUE ^
     -D LUE_HAVE_BOOST=TRUE ^
@@ -25,10 +22,10 @@ cmake %SRC_DIR% ^
     -D LUE_HAVE_PYBIND11=TRUE ^
     -D HPX_IGNORE_COMPILER_COMPATIBILITY=TRUE ^
     -D Python3_EXECUTABLE="%PYTHON%"
-if %errorlevel% neq 0 exit /b %errorlevel%
+if errorlevel 1 exit 1
 
-cmake --build . --target all --parallel 1
-if %errorlevel% neq 0 exit /b %errorlevel%
+cmake --build . --config Release --parallel %CPU_COUNT%
+if errorlevel 1 exit 1
 
 cmake --install . --component lue_runtime
-if %errorlevel% neq 0 exit /b %errorlevel%
+if errorlevel 1 exit 1
