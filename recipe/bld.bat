c7a07554c675e15a6681bda10c92ecddb3278df6
Kor de Jong <k.dejong1@uu.nl>
2024-06-05 16:40:52 +0200

    Prepare for 0.3.8 release

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,31 +1,58 @@
-mkdir build
-cd build
-if errorlevel 1 exit 1
+echo on
 
-cmake .. ^
-    -G "NMake Makefiles" ^
-    -D CMAKE_BUILD_TYPE=Release ^
-    -D CMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+mkdir build
+if errorlevel 1 exit /b 1
+
+%PYTHON% environment/script/write_conan_profile.py %CXX% host_profile
+if errorlevel 1 exit /b 1
+
+%PYTHON% environment/script/write_conan_profile.py %CXX% build_profile
+if errorlevel 1 exit /b 1
+
+set LUE_CONAN_PACKAGES=imgui
+
+conan install . ^
+    --profile:host=host_profile ^
+    --profile:build=build_profile ^
+    --build=missing ^
+    --output-folder=build
+if errorlevel 1 exit /b 1
+
+:: Unset CMAKE_GENERATOR_PLATFORM. The platform is already specified in Conan's preset. Otherwise:
+:: Generator
+::     Ninja
+:: does not support platform specification, but platform
+::     x64
+:: was specified.
+set CMAKE_GENERATOR_PLATFORM=
+
+:: Unset CMAKE_GENERATOR_TOOLSET. Otherwise:
+:: Generator
+::   Ninja
+:: does not support toolset specification, but toolset
+::   v142
+:: was specified.
+set CMAKE_GENERATOR_TOOLSET=
+
+set CMAKE_PREFIX_PATH=build
+
+cmake --preset conan-release ^
+    %CMAKE_ARGS% ^
     -D CMAKE_INSTALL_LIBDIR=lib ^
     -D LUE_INSTALL_PYTHON_PACKAGE_DIR="%SP_DIR%/lue" ^
     -D LUE_DATA_MODEL_WITH_PYTHON_API=TRUE ^
     -D LUE_DATA_MODEL_WITH_UTILITIES=TRUE ^
-    -D LUE_FRAMEWORK_WITH_PYTHON_API=TRUE ^
+    -D LUE_BUILD_VIEW=TRUE ^
     -D LUE_BUILD_QA=TRUE ^
     -D LUE_QA_WITH_PYTHON_API=TRUE ^
-    -D LUE_HAVE_BOOST=TRUE ^
-    -D LUE_HAVE_DOCOPT=TRUE ^
-    -D LUE_HAVE_FMT=TRUE ^
-    -D LUE_HAVE_GDAL=TRUE ^
-    -D LUE_HAVE_HDF5=TRUE ^
-    -D LUE_HAVE_NLOHMANN_JSON=TRUE ^
-    -D LUE_HAVE_PYBIND11=TRUE ^
+    -D LUE_FRAMEWORK_WITH_IMAGE_LAND=TRUE ^
+    -D LUE_FRAMEWORK_WITH_PYTHON_API=TRUE ^
     -D HPX_IGNORE_COMPILER_COMPATIBILITY=TRUE ^
-    -D Python3_EXECUTABLE="%PYTHON%"
-if errorlevel 1 exit 1
+    -D Python_EXECUTABLE="%PYTHON%"
+if errorlevel 1 exit /b 1
 
-cmake --build . --config Release --parallel %CPU_COUNT%
-if errorlevel 1 exit 1
+cmake --build build --config Release --target all
+if errorlevel 1 exit /b 1
 
-cmake --install . --component lue_runtime
-if errorlevel 1 exit 1
+cmake --install build --config Release --component lue_runtime
+if errorlevel 1 exit /b 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,38 +1,39 @@
 #!/usr/bin/env bash
 set -e
 
+
 mkdir build
-cd build
 
-lue_build_view="TRUE"
-
-if [[ "$target_platform" == "osx-64" ]]; then
-    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+if [[ $target_platform == osx* ]]; then
     export CXXFLAGS="${CXXFLAGS} -DTARGET_OS_OSX"
+
+    if [[ $target_platform == "osx-64" ]]; then
+        export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+    fi
 fi
 
-if [[ "$target_platform" == "osx-arm64" ]]; then
-    export CXXFLAGS="${CXXFLAGS} -DTARGET_OS_OSX"
-    # Conan fails to build imgui
-    lue_build_view="FALSE"
-fi
+${PYTHON} environment/script/write_conan_profile.py ${CXX} host_profile
+${PYTHON} environment/script/write_conan_profile.py ${CXX} build_profile
 
-cmake \
-    .. \
-    -G "Ninja" \
-    ${CMAKE_ARGS} \
-    -D CMAKE_BUILD_TYPE="Release" \
-    -D LUE_INSTALL_PYTHON_PACKAGE_DIR="${SP_DIR}/lue" \
-    -D LUE_HAVE_DOCOPT:BOOL=TRUE \
-    -D LUE_HAVE_MS_GSL:BOOL=FALSE \
-    -D LUE_DATA_MODEL_WITH_PYTHON_API=TRUE \
-    -D LUE_DATA_MODEL_WITH_UTILITIES=TRUE \
-    -D LUE_BUILD_QA=TRUE \
-    -D LUE_QA_WITH_PYTHON_API=TRUE \
-    -D LUE_FRAMEWORK_WITH_PYTHON_API=TRUE \
-    -D LUE_BUILD_VIEW=$lue_build_view \
-    -D HPX_IGNORE_COMPILER_COMPATIBILITY=TRUE \
-    -D Python3_EXECUTABLE="${PYTHON}"
+LUE_CONAN_PACKAGES="imgui" conan install . \
+    --profile:host=host_profile \
+    --profile:build=build_profile \
+    --build=missing \
+    --output-folder=build
 
-cmake --build . --target all
-cmake --install . --component lue_runtime
+CMAKE_PREFIX_PATH=build \
+    cmake --preset conan-release \
+        ${CMAKE_ARGS} \
+        -D LUE_INSTALL_PYTHON_PACKAGE_DIR="${SP_DIR}/lue" \
+        -D LUE_DATA_MODEL_WITH_PYTHON_API=TRUE \
+        -D LUE_DATA_MODEL_WITH_UTILITIES=TRUE \
+        -D LUE_BUILD_VIEW=TRUE \
+        -D LUE_BUILD_QA=TRUE \
+        -D LUE_QA_WITH_PYTHON_API=TRUE \
+        -D LUE_FRAMEWORK_WITH_IMAGE_LAND=TRUE \
+        -D LUE_FRAMEWORK_WITH_PYTHON_API=TRUE \
+        -D HPX_IGNORE_COMPILER_COMPATIBILITY=TRUE \
+        -D Python_EXECUTABLE="${PYTHON}"
+
+cmake --build build --config Release --target all
+cmake --install build --config Release --component lue_runtime
